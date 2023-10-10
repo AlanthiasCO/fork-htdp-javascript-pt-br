6182c59ec53bc5dc8c16c4545798c39382b5dc4b
Alan <alan.mathias@ufpr.br>
2023-10-10 15:52:20 -0300

    Inserindo as constantes do personagem principal

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,5 @@
 import { Imagem, ModoImagem, carregarImagem, cenaVazia, circulo, colocarImagem, elipse, encima, folhaTransparente, lado, quadrado, retangulo, rotacionar, sobrepor, texto } from "../lib/image"
 import { reactor } from "../lib/universe";
+
+ALTURA = 900;
+LARGURA = 900;
--- src/trabalho/main.ts
+++ src/trabalho/main.ts
@@ -1,6 +1,21 @@
-import { cenaVazia } from "../../lib/image"
+import { alturaImagem, carregarImagem, cenaVazia, larguraImagem } from "../../lib/image"
+import imgUrlPersonagem from "./samurai_padrao.png";
 
-const [LARGURA, ALTURA] = [600, 400]
 
-const TELA = cenaVazia(LARGURA, ALTURA)
- 
+//---------TELA-----------------------
+const [LARGURA, ALTURA] = [900, 500]
+const TELA = cenaVazia(LARGURA, ALTURA);
+//------------------------------------ 
+
+//---------PERSONAGEM-----------------------
+const IMG_PERSONAGEM_PADRAO = carregarImagem(imgUrlPersonagem, 120, 120);
+const Y_INICIAL_PERSONAGEM = 250;
+const X_INICIAL_PERSONAGEM = 450;
+const VELOCIDADE_PERSONAGEM = 3;
+//--------LIMITES_PERSONAGEM-----------------
+const LIMITE_ESQUERDA_PERSONAGEM = 0 + larguraImagem(IMG_PERSONAGEM_PADRAO) / 2;
+const LIMITE_DIREITA_PERSONAGEM = LARGURA - larguraImagem(IMG_PERSONAGEM_PADRAO) / 2;
+const LIMITE_BAIXO_PERSONAGEM = ALTURA - alturaImagem(IMG_PERSONAGEM_PADRAO) / 2;
+const LIMITE_CIMA_PERSONAGEM = 0 + alturaImagem(IMG_PERSONAGEM_PADRAO) / 2;
+
+
